5c446f3ea35f81d84f6268d742837557cfb25bda
Alexander Zobnin <alexanderzobnin@gmail.com>
2015-11-18 21:28:58 +0300

    Modified grunt watch task for rebuilding only typescript and less files.

--- tasks/options/watch.js
+++ tasks/options/watch.js
@@ -2,38 +2,29 @@ module.exports = function(config) {
   'use strict';
 
   return {
-    // css: {
-    //   files: [ '<%= srcDir %>/less#<{(||)}>#*.less' ],
-    //   tasks: ['css'],
-    //   options: {
-    //     spawn: false
-    //   }
-    // },
-
-    copy_to_gen: {
-      files: ['<%= srcDir %>/**/*'],
-      tasks: [
-        'clean:gen',
-        'copy:public_to_gen',
-        'css',
-        'typescript:build',
-        'jshint',
-        'jscs',
-        'tslint',
-        'karma:test'
-      ],
+    css: {
+      files: [ '<%= srcDir %>/less/**/*.less' ],
+      tasks: ['css'],
       options: {
         spawn: false
       }
     },
 
-    // typescript: {
-    //   files: ['<%= srcDir %>/app#<{(||)}>#*.ts', '<%= srcDir %>/test#<{(||)}>#*.ts'],
-    //   tasks: ['tslint', 'typescript:build'],
-    //   options: {
-    //     spawn: false
-    //   }
-    // }
+    copy_to_gen: {
+      files: ['<%= srcDir %>/**/*', '!<%= srcDir %>/**/*.less'],
+      tasks: ['copy:public_to_gen'],
+      options: {
+        spawn: false
+      }
+    },
+
+    typescript: {
+      files: ['<%= srcDir %>/app/**/*.ts'],
+      tasks: ['typescript:build'],
+      options: {
+        spawn: false
+      }
+    }
 
   };
 };
