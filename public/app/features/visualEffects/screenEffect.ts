ca9b2185ed87763773972736e83d8634862f6259
Alexander Zobnin <alexanderzobnin@gmail.com>
2023-02-28 10:18:19 +0100

    Lanczos resize WIP

--- public/app/features/visualEffects/screenEffect.ts
+++ public/app/features/visualEffects/screenEffect.ts
@@ -1,5 +1,12 @@
 import { CanvasEffect, CanvasEffectOptions } from './canvasEffect';
-import { getImageDefinition, getDefinitionPoint, setDefinitionPoint, gaussianRandom } from './utils';
+import {
+  getImageDefinition,
+  getDefinitionPoint,
+  setDefinitionPoint,
+  gaussianRandom,
+  LanczosFilter,
+  convertImageGamma,
+} from './utils';
 
 export interface CanvasEffectScreenOptions extends CanvasEffectOptions {
   cellSize: number;
@@ -26,8 +33,6 @@ export class CanvasEffectScreen extends CanvasEffect {
     const canvasWidth = canvas.width;
     const canvasHeight = canvas.height;
     const cellSize = this.cellSize;
-    const intermediateWidth = 640;
-    const intermediateHeight = 480;
 
     const ctx = canvas.getContext('2d');
 
@@ -42,82 +47,31 @@ export class CanvasEffectScreen extends CanvasEffect {
     // const initialDefSize = definition.reduce((acc, curr) => acc + curr, 0);
 
     let image = ctx?.getImageData(0, 0, canvasWidth, canvasHeight);
-    const resizedImageData = resizeImage(image.data, image.width, image.height, intermediateHeight);
-    const resizedImageWidth = resizedImageData.length / 4 / intermediateHeight;
-    const resizedImage = new ImageData(resizedImageData, resizedImageWidth);
-    console.log(resizedImage.width, resizedImage.height, resizedImage.data.length);
-    ctx.putImageData(resizedImage, 0, 0);
-
-    // const width = image.width;
-    // const height = image.height;
-    // ctx.clearRect(0, 0, image.width, image.height);
-    // ctx.fillStyle = 'rgb(0, 0, 0)';
-    // ctx.fillRect(0, 0, image.width, image.height);
-    // let yShift = 0;
-
-    // for (let i = 0; i < height; i += cellSize) {
-    //   for (let j = 0; j < width; j += cellSize) {
-    //     const cellPixels = getCellPixels(image.data, width, height, cellSize, j, i);
-    //     const p = getAverageColor(cellPixels);
-    //     this.cells.push([...p, j, i]);
-
-    //     ctx.fillStyle = `rgb(${p[0]}, ${p[1]}, ${p[2]})`;
-    //     ctx.fillRect(j, i, cellSize, cellSize);
-
-    //     const modifier = (Math.random() + 10) / 10;
-    //     // const pixelWidth = Math.floor(cellSize / 3);
-    //     const pixelWidth = 1;
-    //     const bloom = 600 / 255;
-    //     const alphaNorm = Math.min((p[3] / 255) * bloom, 255);
-    //     yShift = yShift === 0 ? 1 : 0;
-
-    //     // ctx.fillStyle = `rgb(${p[0] * alphaNorm * modifier * bloom}, 0, 0)`;
-    //     // ctx.fillRect(j, i + yShift, pixelWidth, cellSize - 1);
-    //     // ctx.fillStyle = `rgb(0, ${p[1] * alphaNorm * modifier * bloom}, 0)`;
-    //     // ctx.fillRect(j + 2, i + yShift, pixelWidth, cellSize - 1);
-    //     // ctx.fillStyle = `rgb(0, 0, ${p[2] * alphaNorm * modifier * bloom})`;
-    //     // ctx.fillRect(j + 4, i + yShift, pixelWidth, cellSize - 1);
-
-    //     const lightCoef = 1.5;
-    //     const darkCoef = 0.5;
-    //     const darkCoef2 = darkCoef * darkCoef;
-
-    //     // ctx.beginPath();
-    //     // ctx.strokeStyle = `rgba(${p[0] * darkCoef2},${p[1] * darkCoef2},${p[2] * darkCoef2},${p[3] * darkCoef2})`;
-    //     // // ctx.arc(j, i, cellSize / 2, 0, 2 * Math.PI);
-    //     // // ctx.fill();
-    //     // ctx.moveTo(j, i);
-    //     // ctx.lineTo(j + cellSize - 1, i);
-    //     // ctx.lineTo(j + cellSize - 1, i + cellSize - 1);
-    //     // ctx.stroke();
-
-    //     // ctx.strokeStyle = `rgba(${p[0] * darkCoef},${p[1] * darkCoef},${p[2] * darkCoef},${p[3] * darkCoef})`;
-    //     // ctx.beginPath();
-    //     // ctx.moveTo(j + cellSize - 1, i + cellSize - 1);
-    //     // ctx.lineTo(j, i + cellSize - 1);
-    //     // ctx.lineTo(j, i);
-    //     // ctx.stroke();
-
-    //     // ctx.fillStyle = `rgba(${p[0] * darkCoef},${p[1] * darkCoef},${p[2] * darkCoef},${p[3] * darkCoef})`;
-    //     // ctx.fillRect(j, i, 1, 1);
-    //     // ctx.fillStyle = `rgba(${p[0] * 1.5},${p[1] * 1.5},${p[2] * 1.5},${p[3] * 1.5})`;
-    //     // ctx.fillRect(j + cellSize - 1, i, 1, 1);
-    //     // ctx.fillStyle = `rgba(${p[0] * 1.5},${p[1] * 1.5},${p[2] * 1.5},${p[3] * 1.5})`;
-    //     // ctx.fillRect(j, i + cellSize - 1, 1, 1);
-    //     // ctx.fillStyle = `rgba(${p[0] * 1.5},${p[1] * 1.5},${p[2] * 1.5},${p[3] * 1.5})`;
-    //     // ctx.fillRect(j + cellSize - 1, i + cellSize - 1, 1, 1);
-    //   }
-    // }
-
-    // for (let i = 0; i < cells.length; i++) {
-    //   const p = cells[i];
-    //   const y = Math.floor((i / width) * 4);
-    //   const x = (i - (width / 4) * y) * 4;
-    //   ctx.fillStyle = `rgba(${p[0]},${p[1]},${p[2]},${p[3]})`;
-    //   ctx.fillRect(x, y, cellSize, cellSize);
-    // }
+    const crtImage = this.crtFilter(image);
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+    ctx.putImageData(crtImage, 0, 0);
 
     // this.animationFrame(0, ctx, image);
+  }
+
+  crtFilter(image: ImageData): ImageData {
+    // 640x480
+    const intermediateHeight = 480;
+    const scanLinesCount = Math.ceil(image.height / intermediateHeight) * intermediateHeight;
+    const reScaleFactor = scanLinesCount / image.height;
+    const reScaleHeight = scanLinesCount;
+    const reScaleWidth = Math.floor(image.width * reScaleFactor);
+    // const intermediateWidth = Math.floor(image.width * scaleFactor);
+
+    const lanczos = new LanczosFilter(2);
+    const startTs = performance.now();
+    const resizedImage = lanczos.resize(image.data, image.width, image.height, reScaleWidth, reScaleHeight);
+    console.log(`Resize time: ${Math.floor(performance.now() - startTs)} ms`);
+    console.log(resizedImage.width, resizedImage.height, resizedImage.data.length);
+    convertImageGamma(resizedImage, 2);
+    console.log(`Total time: ${Math.floor(performance.now() - startTs)} ms`);
+
+    return resizedImage;
   }
 
   animationFrame(ts: number, ctx: CanvasRenderingContext2D | null, image: ImageData) {
@@ -187,69 +141,4 @@ function getCellPixels(
     }
   }
   return pixels;
-}
-
-function resizeImage(
-  imageData: Uint8ClampedArray,
-  width: number,
-  height: number,
-  destHeight: number
-): Uint8ClampedArray {
-  const resizedData: number[] = [];
-  const resizeFactor = height / destHeight;
-  const resizeFactorFloor = Math.floor(resizeFactor);
-  const cellSize = Math.ceil(resizeFactor);
-  const rowSize = width * 4;
-  if (resizeFactor <= 1) {
-    return imageData;
-  }
-
-  const destWidth = Math.floor(width / resizeFactor);
-
-  // Go through rows
-  let iRow = 0;
-  let i = 0;
-  while (iRow < imageData.length / rowSize - resizeFactorFloor) {
-    for (let j = 0; j < rowSize; j++) {
-      let newColor = 0;
-      for (let k = 0; k < cellSize; k++) {
-        let colorWeight = 1;
-        if (k > resizeFactorFloor) {
-          colorWeight = resizeFactor - resizeFactorFloor;
-        }
-        newColor += imageData[i + rowSize * k + j] * colorWeight;
-      }
-      resizedData.push(newColor);
-    }
-    iRow += resizeFactor;
-    i = Math.floor(iRow) * rowSize;
-  }
-
-  console.log(resizedData.length / destHeight / 4);
-  console.log(width);
-  console.log(resizedData.length / width / 4);
-  // Go through columns
-  const resizedColumns: number[] = [];
-  for (let j = 0; j < resizedData.length; j += rowSize) {
-    let iCol = 0;
-    i = 0;
-    while (i < rowSize - resizeFactorFloor * cellSize * 4) {
-      for (let colorIdx = 0; colorIdx < 4; colorIdx++) {
-        let newColor = 0;
-        for (let k = 0; k < cellSize; k++) {
-          let colorWeight = 1;
-          if (k > resizeFactorFloor) {
-            colorWeight = resizeFactor - resizeFactorFloor;
-          }
-          newColor += resizedData[j + i + k * 4 + colorIdx] * colorWeight;
-        }
-        resizedColumns.push(newColor);
-      }
-      iCol += resizeFactor;
-      i = Math.floor(iCol) * 4;
-    }
-  }
-
-  const resized = new Uint8ClampedArray(resizedColumns);
-  return resized;
 }
--- public/app/features/visualEffects/utils.ts
+++ public/app/features/visualEffects/utils.ts
@@ -83,3 +83,197 @@ export function gaussianRandom(): number {
   // Resample if out of range
   return z >= 0 && z <= 1 ? z : gaussianRandom();
 }
+
+export class LanczosFilter {
+  lanczosLobes: number;
+  scaleX: number;
+  scaleY: number;
+  rcpScaleX: number;
+  rcpScaleY: number;
+
+  constructor(lanczosLobes = 3) {
+    this.lanczosLobes = lanczosLobes;
+    this.scaleX = 1;
+    this.scaleY = 1;
+    this.rcpScaleX = 1 / this.scaleX;
+    this.rcpScaleY = 1 / this.scaleY;
+  }
+
+  lanczosCreate(lobes: number) {
+    return (x: number) => {
+      if (x >= lobes || x <= -lobes) {
+        return 0.0;
+      }
+      if (x < 1.1920929e-7 && x > -1.1920929e-7) {
+        return 1.0;
+      }
+      x *= Math.PI;
+      const xx = x / lobes;
+      return ((Math.sin(x) / x) * Math.sin(xx)) / xx;
+    };
+  }
+
+  // Implementation from fabric.js
+  // https://github.com/fabricjs/fabric.js/blob/master/src/filters/Resize.ts
+  resize(srcData: Uint8ClampedArray, oW: number, oH: number, dW: number, dH: number) {
+    const scaleX = dW / oW;
+    const scaleY = dH / oH;
+    this.rcpScaleX = 1 / scaleX;
+    this.rcpScaleY = 1 / scaleY;
+
+    const destImg = new ImageData(dW, dH);
+    const destData = destImg.data;
+    const lanczos = this.lanczosCreate(this.lanczosLobes);
+    const ratioX = this.rcpScaleX;
+    const ratioY = this.rcpScaleY;
+    const rcpRatioX = 2 / this.rcpScaleX;
+    const rcpRatioY = 2 / this.rcpScaleY;
+    const range2X = Math.ceil((ratioX * this.lanczosLobes) / 2);
+    const range2Y = Math.ceil((ratioY * this.lanczosLobes) / 2);
+    const center: { x?: number; y?: number } = {};
+    const icenter: { x?: number; y?: number } = {};
+
+    for (let u = 0; u < dW; u++) {
+      let v: number;
+      let i: number;
+      let weight: number;
+      let idx: number;
+      let a: number;
+      let red: number;
+      let green: number;
+      let blue: number;
+      let alpha: number;
+      let fX: number;
+      let fY: number;
+
+      center.x = (u + 0.5) * ratioX;
+      icenter.x = Math.floor(center.x);
+
+      for (v = 0; v < dH; v++) {
+        center.y = (v + 0.5) * ratioY;
+        icenter.y = Math.floor(center.y);
+        a = 0;
+        red = 0;
+        green = 0;
+        blue = 0;
+        alpha = 0;
+        for (i = icenter.x - range2X; i <= icenter.x + range2X; i++) {
+          if (i < 0 || i >= oW) {
+            continue;
+          }
+          fX = Math.floor(1000 * Math.abs(i - center.x));
+          for (let j = icenter.y - range2Y; j <= icenter.y + range2Y; j++) {
+            if (j < 0 || j >= oH) {
+              continue;
+            }
+            fY = Math.floor(1000 * Math.abs(j - center.y));
+            weight = lanczos(Math.sqrt(Math.pow(fX * rcpRatioX, 2) + Math.pow(fY * rcpRatioY, 2)) / 1000);
+            if (weight > 0) {
+              idx = (j * oW + i) * 4;
+              a += weight;
+              red += weight * srcData[idx];
+              green += weight * srcData[idx + 1];
+              blue += weight * srcData[idx + 2];
+              alpha += weight * srcData[idx + 3];
+            }
+          }
+        }
+        idx = (v * dW + u) * 4;
+        destData[idx] = red / a;
+        destData[idx + 1] = green / a;
+        destData[idx + 2] = blue / a;
+        destData[idx + 3] = alpha / a;
+      }
+    }
+
+    return destImg;
+  }
+}
+
+export function resizeImageSimple(
+  imageData: Uint8ClampedArray,
+  width: number,
+  height: number,
+  destHeight: number
+): Uint8ClampedArray {
+  const resizedData: number[] = [];
+  const resizeFactor = height / destHeight;
+  const resizeFactorFloor = Math.floor(resizeFactor);
+  const cellSize = Math.ceil(resizeFactor);
+  const rowSize = width * 4;
+  if (resizeFactor <= 1) {
+    return imageData;
+  }
+
+  const destWidth = Math.floor(width / resizeFactor);
+
+  // Go through rows
+  let iRow = 0;
+  let i = 0;
+  while (iRow < imageData.length / rowSize - resizeFactorFloor) {
+    for (let j = 0; j < rowSize; j++) {
+      let newColor = 0;
+      for (let k = 0; k < cellSize; k++) {
+        let colorWeight = 1;
+        if (k > resizeFactorFloor) {
+          colorWeight = resizeFactor - resizeFactorFloor;
+        }
+        newColor += imageData[i + rowSize * k + j] * colorWeight;
+      }
+      newColor = Math.round(newColor / cellSize);
+      resizedData.push(newColor);
+    }
+    iRow += resizeFactor;
+    i = Math.floor(iRow) * rowSize;
+  }
+
+  // Go through columns
+  const resizedColumns: number[] = [];
+  let rowIdx = 0;
+  for (let j = 0; j < resizedData.length; j += rowSize) {
+    let iCol = 0;
+    i = 0;
+    const magnitude = scanlineMagnitude(rowIdx);
+    console.log(magnitude);
+    while (i < rowSize - resizeFactorFloor * cellSize * 4) {
+      for (let colorIdx = 0; colorIdx < 4; colorIdx++) {
+        let newColor = 0;
+        for (let k = 0; k < cellSize; k++) {
+          let colorWeight = 1;
+          if (k > resizeFactorFloor) {
+            colorWeight = resizeFactor - resizeFactorFloor;
+          }
+          newColor += resizedData[j + i + k * 4 + colorIdx] * colorWeight;
+        }
+        newColor = Math.round(newColor / cellSize);
+        newColor = convertGamma(newColor, 2);
+
+        newColor = magnitude * newColor;
+        resizedColumns.push(newColor);
+      }
+      iCol += resizeFactor;
+      i = Math.floor(iCol) * 4;
+    }
+    rowIdx++;
+  }
+
+  const resized = new Uint8ClampedArray(resizedColumns);
+  return resized;
+}
+
+export function convertImageGamma(imageData: ImageData, gamma: number) {
+  for (let i = 0; i < imageData.data.length; i++) {
+    imageData.data[i] = convertGamma(imageData.data[i], gamma);
+  }
+  return imageData;
+}
+
+// Converting into linear colors (invert gamma correction)
+function convertGamma(v: number, gamma: number) {
+  return Math.pow(v / 255, 1 / gamma) * 255;
+}
+
+export function scanlineMagnitude(n: number) {
+  const c = 0.3;
+  return Math.exp(-(((n - 0.5) * (n - 0.5)) / (2 * c * c)));
+}
